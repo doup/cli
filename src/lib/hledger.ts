85702fcaba458c619d25e01210e3053871bac4e8
doup <asier@illarra.com>
2019-12-08 18:50:07 +0100

    Move types to another file

--- src/lib/hledger.ts
+++ src/lib/hledger.ts
@@ -1,63 +1,17 @@
-export interface Amount {
-    total: number;
-    currency: string;
-}
-
-export type PostingAmount = Amount | number | string;
-
-export interface Posting {
-    account: string;
-    amount?: PostingAmount;
-    dateValue?: string;
-    foreignAmount?: number;
-    foreignCurrency?: string;
-}
-
-export interface PostingHalf {
-    half: string;
-}
-
-type PostingEntry = Posting | PostingHalf;
-
-export interface AccountAssertion {
-    account: string;
-    amount: PostingAmount;
-}
-
-export interface AssertAccount {
-    date: string;
-    assert: AccountAssertion[];
-}
-
-export interface Transaction {
-    date: string;
-    ignore?: boolean;
-    item: string;
-    tags?: string[];
-    postings: PostingEntry[];
-}
-
-export type JournalEntry = AssertAccount | Transaction;
-
-function isAmount(object: any): object is Amount {
-    return typeof object === 'object' && ('total' in object) && ('currency' in object);
-}
-
-function isPosting(object: any): object is Posting {
-    return 'account' in object;
-}
-
-function isPostingHalf(object: any): object is PostingHalf {
-    return 'half' in object;
-}
-
-function isAssertAccount(object: any): object is AssertAccount {
-    return 'assert' in object;
-}
-
-function isTransaction(object: any): object is Transaction {
-    return 'postings' in object;
-}
+import {
+    Amount,
+    PostingAmount,
+    AssertAccount,
+    Transaction,
+    Posting,
+    JournalEntry,
+    isTransaction,
+    isAssertAccount,
+    isPostingHalf,
+    isPosting,
+    PostingHalf,
+    isAmount,
+} from './types';
 
 function isNumeric(num: any) {
     return !isNaN(num);
---
+++ src/lib/types.ts
@@ -0,0 +1,60 @@
+export interface Amount {
+    total: number;
+    currency: string;
+}
+
+export type PostingAmount = Amount | number | string;
+
+export interface Posting {
+    account: string;
+    amount?: PostingAmount;
+    dateValue?: string;
+    foreignAmount?: number;
+    foreignCurrency?: string;
+}
+
+export interface PostingHalf {
+    half: string;
+}
+
+type PostingEntry = Posting | PostingHalf;
+
+export interface AccountAssertion {
+    account: string;
+    amount: PostingAmount;
+}
+
+export interface AssertAccount {
+    date: string;
+    assert: AccountAssertion[];
+}
+
+export interface Transaction {
+    date: string;
+    ignore?: boolean;
+    item: string;
+    tags?: string[];
+    postings: PostingEntry[];
+}
+
+export type JournalEntry = AssertAccount | Transaction;
+
+export function isAmount(object: any): object is Amount {
+    return typeof object === 'object' && ('total' in object) && ('currency' in object);
+}
+
+export function isPosting(object: any): object is Posting {
+    return 'account' in object;
+}
+
+export function isPostingHalf(object: any): object is PostingHalf {
+    return 'half' in object;
+}
+
+export function isAssertAccount(object: any): object is AssertAccount {
+    return 'assert' in object;
+}
+
+export function isTransaction(object: any): object is Transaction {
+    return 'postings' in object;
+}
